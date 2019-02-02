77a79dcf216d5a1d887119791ae6c595c41e7256
Dims_M <dimsxm@yandex.ru>
2019-02-02 21:28:36 +0300

    Добавьте файлы проекта.

---
+++ PotokiConsoleApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PotokiConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+
+       
+
+    }
+
+
+    /// <summary>
+    /// Работаем с потоками
+    /// </summary>
+    class RabPotoki
+    {
+
+    }
+
+}
